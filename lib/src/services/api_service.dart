afa02e584da72f6b7e7d2b04054c2c25170eb6be
luizfiuzaa <luizffreitas2006@gmail.com>
2024-07-31 08:02:12 -0300

    arrumando uri api

--- lib/src/services/api_service.dart
+++ lib/src/services/api_service.dart
@@ -7,7 +7,7 @@ class ApiService {
   final Dio _dio = Dio();
 
   ApiService() {
-    _dio.options.baseUrl = 'https://api.openai.com/v1/engines/davinci/completions';
+    _dio.options.baseUrl = 'https://api.openai.com/v1/chat/completions';
     _dio.options.headers['Authorization'] =
         'Bearer ${dotenv.env['OPENAI_API_KEY']}';
     _dio.options.headers['Content-Type'] = 'application/json';
@@ -18,7 +18,14 @@ class ApiService {
       final response = await _dio.post(
         '',
         data: {
+          "model": "gpt-3.5-turbo",
           "prompt": prompt,
+          "messages": [
+            {
+              "role": "user",
+              "content": prompt,
+            },
+          ],
           "max_tokens": 100,
         },
       );
